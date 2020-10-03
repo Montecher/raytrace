b46f4bd09f95e138e277728f21780242a01adefb
_Ara0n_ <thomas99.montero@gmail.com>
2020-10-03 09:54:31 +0200

    fixed constructors and added translation

--- src/Object.cpp
+++ src/Object.cpp
@@ -21,13 +21,13 @@ Intersection::Intersection(const Object* obj1, const Object* obj2) {
 }
 
 Intersection::Intersection(const Object* obj1, const Object* obj2, const Object* obj3) {
-    this->obj1 = Intersection(obj1, obj2);
+    this->obj1 = new Intersection(obj1, obj2);
     this->obj2 = obj3;
 }
 
 Intersection::Intersection(const Object* obj1, const Object* obj2, const Object* obj3, const Object* obj4) {
-    this->obj1 = Intersection(obj1, obj2);
-    this->obj2 = Intersection(obj3, obj4);
+    this->obj1 = new Intersection(obj1, obj2);
+    this->obj2 = new Intersection(obj3, obj4);
 }
 
 Intersection::~Intersection() {
@@ -46,13 +46,13 @@ Union::Union(const Object* obj1, const Object* obj2) {
 }
 
 Union::Union(const Object* obj1, const Object* obj2, const Object* obj3) {
-    this->obj1 = Union(obj1, obj2);
+    this->obj1 = new Union(obj1, obj2);
     this->obj2 = obj3;
 }
 
 Union::Union(const Object* obj1, const Object* obj2, const Object* obj3, const Object* obj4) {
-    this->obj1 = Union(obj1, obj2);
-    this->obj2 = Union(obj3, obj4);
+    this->obj1 = new Union(obj1, obj2);
+    this->obj2 = new Union(obj3, obj4);
 }
 
 Union::~Union() {
@@ -90,4 +90,18 @@ Exclusion::~Exclusion() {
 
 double Exclusion::distance_to(const Vec3& point) const {
     return std::max(obj1->distance_to(point), -obj2->distance_to(point));
+}
+
+
+Translation::Translation(const Object* obj, const Vec3 translate) {
+    this->obj = obj;
+    this->translate = translate;
+}
+
+Translation::~Translation() {
+    delete this->obj;
+}
+
+double Translation::distance_to(const Vec3& point) const {
+    return obj->distance_to(point - translate);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -14,16 +14,18 @@ using namespace std;
 
 int main(){
 
-    Sphere s1 = Sphere(4, 0, 0, 1);
-    Sphere s2 = Sphere(3, 1, 1, 0.5);
-    Plane navion = Plane(0, -1, 1, 3);
-    Box boiboite = Box(3.5, -0.825, 0.825, 4.5, -1.375, 1.375);
+    Sphere* s1 = new Sphere(4, 0, 0, 1);
+    Sphere* s2 = new Sphere(3, 1, 1, 0.5);
+    Plane* navion = new Plane(0, -1, 1, 3);
+    Box* boiboite = new Box(3.5, -0.825, 0.825, 4.5, -1.375, 1.375);
+    Translation* decal = new Translation(boiboite, Vec3(0, 1, 0.125));
 
     vector<Object*> scene;
-    scene.push_back(&s1);
-    scene.push_back(&s2);
-    scene.push_back(&navion);
-    scene.push_back(&boiboite);
+    scene.push_back(s1);
+    scene.push_back(s2);
+    scene.push_back(navion);
+    scene.push_back(boiboite);
+    scene.push_back(decal);
 
     Cam cam = Cam();
 
