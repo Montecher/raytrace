33e91826e4181b6a1f8db6f1f820c07a4d360552
_Ara0n_ <thomas99.montero@gmail.com>
2020-10-20 16:48:14 +0200

    started documenting for doxygen

--- src/Window.hh
+++ src/Window.hh
@@ -5,27 +5,54 @@
 #include <QtWidgets>
 #include "Cam.h"
 
+
+/**
+ * @class Window
+ * @brief Window containing the renders.
+ *
+ * This is the Window that the user will be able to use to control the camera, render type and the scene to render.
+*/
 class Window : public QWidget {
     Q_OBJECT
 
 private:
-    QImage *image;
-    QLabel *label;
-    QPushButton *toggleRealistic;
+    QImage *image; ///< Object storing the rendered image.
+    QLabel *label; ///< Container of the image in the window.
+    QPushButton *toggleRealistic; ///< Button used to swap between the shaded and realistic view.
 
-    Cam camera;
-    Object *scene;
+    Cam camera; ///< Camera used as the PoV for the rendered image.
+    Object *scene; ///< Scene loaded in the window.
 
 
 public:
+    /**
+     * \brief Constructor for the gui window
+     */
     Window();
 
 private slots:
-    void loadSCene(int);
+    /**
+     * @brief Loads the scene.
+     *
+     * Loads the asked scene, then resets the camera and calls the image renderer.
+     *
+     * @param sceneNb id of the selected value in the combobox with all the scenes listed.
+     */
+    void loadSCene(int sceneNb);
+
     void test(int);
+
+    /**
+     * @brief Image renderer
+     *
+     * Creates the image with the scene and camera stored in the window object attributes with the render mode selected.
+     */
     void createImage();
 
 signals:
+    /**
+     * @brief Signal used to tell the window to refresh the image inside the label.
+     */
     void showImage(QPixmap);
 };
 
