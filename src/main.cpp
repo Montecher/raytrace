99a27a4830fd5dd9d86ccbf6d4a1a827148d4795
Codinget <nathan.decher@gmail.com>
2020-10-21 18:09:55 +0200

    added renderer in comments in main

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,35 @@
 #include "Window.hh"
 #include "Image.h"
 
+/*
+#include <fstream>
+#include "Cam.h"
+#include "scenes.h"
+
+#define SIZE 480
+#define RPP 500
+
+void render(std::string file, Object* scene) {
+    Cam* cam = new Cam();
+    cam->setPos(Vec3::Z*3. - Vec3::X*5.);
+    cam->setPointing(Vec3::O);
+
+    std::fstream fs;
+    fs.open(file, std::fstream::out);
+    fs << cam->render_realistic(scene, SIZE, SIZE, RPP).to_bmp();
+    fs.close();
+
+    delete cam;
+}
+
+int main() {
+    render("out/scene1.bmp", SCENE::scene1());
+    render("out/scene2.bmp", SCENE::scene2());
+    render("out/scene3.bmp", SCENE::scene3());
+    return 0;
+}
+*/
+
 
 int main(int argc, char *argv[])
 {
@@ -14,3 +43,4 @@ int main(int argc, char *argv[])
 
     return app.exec();
 }
+
