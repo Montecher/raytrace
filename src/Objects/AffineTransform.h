edc6fd5ffeb5cfa37db95847ceeea7f44530dbe5
Codinget <nathan.decher@gmail.com>
2020-10-29 15:48:25 +0100

    removed duplicate helpers from AffineTransform

--- src/Objects/AffineTransform.h
+++ src/Objects/AffineTransform.h
@@ -14,13 +14,6 @@ public:
     virtual double distance_to(const Vec3&) const;
     virtual Vec3 normal_at(const Vec3&) const;
     virtual const Material* get_intersecting(const Vec3&) const;
-
-    static Mat3 scale(double, double, double);
-    static Mat3 swapXY, swapXZ, swapYZ;
-    static Mat3 rotateX(double);
-    static Mat3 rotateY(double);
-    static Mat3 rotateZ(double);
-    static Mat3 rotate(double, double, double);
 };
 
 #endif
